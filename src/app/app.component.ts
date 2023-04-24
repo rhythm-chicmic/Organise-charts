339e6ad10a0763f7a2a20485d3bff2ec530e35e0
Rhythm08 <rhythm.sharma@chicmic.co.in>
2023-04-24 12:30:31 +0530

    organise charts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,49 +5,88 @@ import OrgChart from "@balkangraph/orgchart.js";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'orgChart';
-  
+  drop = false
   ngOnInit() {
+    var parentChildRelation = [
+      { from: 7, to: 1, template: 'blue', label: 'Reporting Head' },
+      { from: 6, to: 1, template: 'blue', label: 'Reporting Head' },
+
+    ]
     const tree = document.getElementById('tree');
     if (tree) {
-        var chart = new OrgChart(tree, {
-            nodeBinding: {
-            field_0: "name",
-            img_0: "img"
-            },
-            enableDragDrop:true,
-          
-          
+      OrgChart.CLINK_CURVE = 1.5;
+      var chart = new OrgChart(tree, {
+        nodeBinding: {
+          field_0: "name",
+          img_0: "img"
         },
-       
-    
-        );
-        let nodeData= [
-          { id: 1, name: "Denny Curtis", title: "CEO", img: "https://cdn.balkan.app/shared/2.jpg" },
-          { id: 2, pid: 1, name: "Ashley Barnett", title: "Sales Manager", img: "https://cdn.balkan.app/shared/3.jpg" },
-          { id: 3, pid: 1, name: "Caden Ellison", title: "Dev Manager", img: "https://cdn.balkan.app/shared/4.jpg" },
-          { id: 4, pid: 2, name: "Elliot Patel", title: "Sales", img: "https://cdn.balkan.app/shared/5.jpg" },
-          { id: 5, pid: 2, name: "Lynn Hussain", title: "Sales", img: "https://cdn.balkan.app/shared/6.jpg" },
-          { id: 6, pid: 3, name: "Tanner May", title: "Developer", img: "https://cdn.balkan.app/shared/7.jpg" },
-          { id: 7, pid: 3, name: "Fran Parsons", title: "Developer", img: "https://cdn.balkan.app/shared/8.jpg" }
+        enableDragDrop: this.drop,
+        mode: 'dark',
+        siblingSeparation: 100,
+        levelSeparation: 50,
+        template: "belinda",
+        editForm: {
+          saveAndCloseBtn: 'save',
+          cancelBtn: 'cancel',
+          readOnly: true,
+          buttons: {
+            share: null,
+            pdf: null
+          }
+        },
+        align: OrgChart.align.orientation,
+        clinks: parentChildRelation,
+        mouseScrool: OrgChart.action.none,
+
+        toolbar: {
+          fit: true,
+          layout: true,
+          fullScreen: true,
+
+        }
+
+
+
+
+      },
+
+
+      );
+
+
+
+
+      let nodeData = [
+        { id: 1, name: "Denny Curtis", title: "CEO", img: "https://cdn.balkan.app/shared/2.jpg" },
+        { id: 2, pid: 1, name: "Ashley Barnett", title: "Sales Manager", img: "https://cdn.balkan.app/shared/3.jpg",'Reporting Head': 'Denny Curtis' },
+        { id: 3, pid: 1, name: "Caden Ellison", title: "Dev Manager", img: "https://cdn.balkan.app/shared/4.jpg", 'Reporting Head': 'Denny Curtis' },
+        { id: 4, pid: 2, name: "Elliot Patel", title: "Sales", img: "https://cdn.balkan.app/shared/5.jpg", 'Reporting Head': 'Ashley Barnett' },
+        { id: 5, pid: 2, name: "Lynn Hussain", title: "Sales", img: "https://cdn.balkan.app/shared/6.jpg", 'Reporting Head': 'Ashley Barnett' },
+        { id: 6, pid: 3, name: "Tanner May", title: "Developer", img: "https://cdn.balkan.app/shared/7.jpg", 'Reporting Head': 'Caden Ellison,Denny Curtis' },
+        { id: 7, pid: 3, name: "Fran Parsons", title: "Developer", img: "https://cdn.balkan.app/shared/8.jpg", 'Reporting Head': 'Caden Ellison,Denny Curtis' },
+        // { id: 7, pid: 1, name: "Fran1 Parsons", title: "Developer", img: "https://cdn.balkan.app/shared/8.jpg" }
+
       ];
 
-         chart.load(nodeData);
-        chart.onDrop((res)=>{
-          console.log(res)
-        })
-        chart.onNodeClick((res)=>{
-          console.log(res)
-          console.log(res.node.id)
-          const data = { id: res.node.id, pid: 2, name: "Rahul Patel", title: "Sales", img: "https://cdn.balkan.app/shared/5.jpg" }
-          const curr_id=res.node.id
-         const ans = chart.get(2)
-         console.log(ans)
-        })
-       
+      chart.load(nodeData);
+      chart.onDrop((res) => {
+        console.log(res)
+      })
+      chart.onNodeClick((res) => {
+        console.log(res)
+        console.log(res.node.id)
+        const data = { id: res.node.id, pid: 2, name: "Rahul Patel", title: "Sales", img: "https://cdn.balkan.app/shared/5.jpg" }
+        const curr_id = res.node.id
+        const ans = chart.get(2)
+        console.log(ans)
+      })
+
+
     }
-    
-}
+
+
+  }
 
 }
