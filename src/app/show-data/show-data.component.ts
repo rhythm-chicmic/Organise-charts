a012d2febd20816bc668b083b6be078c279aaae2
Rhythm08 <rhythm.sharma@chicmic.co.in>
2023-04-26 11:54:06 +0530

    minor commit

--- src/app/show-data/show-data.component.ts
+++ src/app/show-data/show-data.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as d3 from "d3";
 import * as d3Hierarchy from 'd3-hierarchy';
 import * as d3Drag from 'd3-drag'
+import  data2 from "../../assets/db.json"
 @Component({
   selector: 'app-show-data',
   templateUrl: './show-data.component.html',
@@ -9,12 +10,16 @@ import * as d3Drag from 'd3-drag'
 })
 export class ShowDataComponent implements OnInit{
   data:any;
+  p=data2
   ngOnInit(){
-    d3.json(
-      " http://localhost:3000/data"
-    ).then((d1:any )=> {
-      console.log(d1)
-        this.data = d1;
-    });
+    this.data = Object.values(this.p)[0]
+ 
+  
+    // d3.json(
+    //   "http://localhost:3000/data"
+    // ).then((d1:any )=> {
+    //   console.log(d1)
+    //     this.data = d1;
+    // });
   }
 }
